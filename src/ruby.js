d8c84445fef9181bcd8e4589c3a153308413987c
robin* <robin_maki@planet.moe>
2018-05-27 17:23:11 +0900

    기본 get 함수 제작

---
+++ src/ruby.js
@@ -0,0 +1,19 @@
+const spec = require('./spec');
+const db = require('./mongo');
+
+function filterByRole(result, roles) {
+    return result;
+}
+
+module.exports = (key) => {
+    const col = db.collection(key);
+    return {
+        async get(_id) {
+            let result = await col.findOne({_id});
+            return filterByRole(result);
+        },
+        async find(query, {limit, skip, sort}) {
+            let result = await col.find(query);
+        }
+    };
+};
