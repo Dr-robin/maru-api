3f3a0fa0420985ff841e0d1b5206b43e6f1b0474
robin* <robin_maki@planet.moe>
2018-05-27 18:07:02 +0900

    validator를 이용해서 일단 get API 구현

--- src/route.js
+++ src/route.js
@@ -1,5 +1,8 @@
 const spec = require('./spec');
 const ruby = require('./ruby');
+const boom = require('boom');
+const joi = require('joi');
+const validator = require('validator');
 
 module.exports = (sv) => {
     Object.keys(spec).forEach((key) => {
@@ -7,7 +10,26 @@ module.exports = (sv) => {
             method: 'GET',
             path: spec[key].endpoint + '/{id}',
             async handler(req) {
-                return await ruby(key).get(req.params.name);
+                let result;
+                if(joi.validate(req.params, {id: joi.string().hex().length(24)})) {
+                    result = await ruby(key).get(req.params.id);
+                }
+                else if(req.params.id && validator.isJSON(req.params.id)) {
+                    /*try {
+                        req.params.id = JSON.parse(req.params.id);
+                    }
+                    catch(e) {
+                        req.params.id = {};
+                    }
+                    result = await ruby(key).find(JSON.parse())*/
+                    throw boom.notFound();
+                }
+                if(result) {
+                    return result;
+                }
+                else {
+                    throw boom.notFound();
+                }
             }
         });
     });
