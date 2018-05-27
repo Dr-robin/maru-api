8b818168830f8b3b87de05ce3bed07fa5c66e1e5
robin* <robin_maki@planet.moe>
2018-05-27 13:18:56 +0900

    설정 파일이 없어도 튕기지는 않도록 수정

--- src/config.js
+++ src/config.js
@@ -2,4 +2,10 @@ const yaml = require('js-yaml');
 const fs = require('fs');
 const path = require('path');
 
-module.exports = yaml.safeLoad(fs.readFileSync(path.resolve(__dirname, '../config.yaml')));
+try {
+    module.exports = yaml.safeLoad(fs.readFileSync(path.resolve(__dirname, '../config.yaml')));
+}
+catch(e) {
+    console.warn(e.toString());
+    module.exports = {};
+}
